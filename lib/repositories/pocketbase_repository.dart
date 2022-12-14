bc5b89468ad4ce584cec2ee3808fda39a4443ac1
Duy Thien <duythien0912@gmail.com>
2022-12-14 10:35:58 +0000

    add http

--- lib/repositories/pocketbase_repository.dart
+++ lib/repositories/pocketbase_repository.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pocketbase/pocketbase.dart';
+import "package:http/http.dart" as http;
 
 import '../models/pocketbase_model.dart';
 import '../provider/pocketbase_provider.dart';
